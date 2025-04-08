837ac2fe66af0e93672a83f5d03158223dfb2a37
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2025-04-08 16:35:35 -0300

    Contagem de doadores

--- henriquerochabomfim/src/app/components/contactForm.jsx
+++ henriquerochabomfim/src/app/components/contactForm.jsx
@@ -15,6 +15,47 @@ export default function ContactForm() {
     window.location.href = mailtoLink;
   };
 
+  const doadores = [
+    "Pedro Pereira Cecílio Ventura",
+    "Stephany Araujo da Silva Lobato",
+    "Nicolas Jesus Silveira",
+    "Thiago de Jesus Santana",
+    "Drielly Santana Farias",
+    "Nereide De Oliveira Costa",
+    "Pedro Henrique Stanzani De Freitas",
+    "Rodney Matheus Santos Fernandes",
+    "Sofia Maria Guimaraes Pavarina",
+    "Julia Diniz Do Nascimento Zanini",
+    "Maria Isabela Marques Pereira",
+    "Giovana Coelho Santana Leite",
+    "Florencio Vente Coha",
+    "Valentina Nieto Garcia",
+    "Leilany De Lima Alencar",
+    "Suzette Amoroso Ribeiro",
+    "Claudio Alves de Carvalho",
+    "Bruno Schmitt Locatelli",
+    "Railson da Silva Nunes",
+    "Adriana Rodrigues Julio",
+    "Flavio Augusto de Maia",
+    "Adriana Fonseca de Souza",
+    "Gustavo Colombi Ribolla",
+    "Mariana Abigair Souza Sabino",
+    "Luiz F A Steinberg",
+    "Guilherme Katayama Kaidei",
+    "Amanda Figueiredo Bittencourt",
+    "Henrique Fazzio Badin",
+    "Larissa Fernandes De Biagi",
+    "Regiane Maria G Rodrigues",
+    "Vanessa Rocha Barbosa Bonfim",
+    "Lavignia Pietra De Souza Christianini",
+    "Eunice Dias Da Fonseca",
+    "Orllei Hernani Amaro Rozauro",
+    "Ana Breatriz Alcântara de Souza",
+    "Adriana Aparecida Ferreira",
+    "Samandra Andraade De Melo",
+    "Ana Paula Da Silva",
+  ];
+
   return (
     <section id="donate" className="p-6 mt-10 text-center">
       <h1 className="text-2xl font-bold py-2">Ajude no Meu Intercâmbio</h1>
@@ -48,87 +89,56 @@ export default function ContactForm() {
         </button>
       </form>
 
-    <div className="p-4">
+      <div className="p-4">
         <h1 className="text-2xl font-bold mb-2">Apoie minha jornada até a Alemanha!</h1>
-            <PixCopyButton />
-    </div>
-
-    <CostTable />
-
-    <DonationsChart />
-
-    {/* Metas */}
-    <h2 className="text-2xl font-bold mb-2 text-center py-2">Metas</h2>
-    <div className="flex justify-center items-center flex-col">
-      <p className="text-sm text-center text-gray-600 mb-2 py-3">
-        ☑ Meta até dia 15/04/2025: alcançar pelo menos 10% do valor arrecadado
-      </p>
-      <p className="text-sm text-center text-gray-600 mb-2 py-3">
-        ⌛Meta até dia 30/04/2025: alcançar pelo menos 20% do valor arrecadado
-      </p>
-    </div>
-
-    {/* Painel de Doadores */}
-    <div className="w-full max-w-4xl mx-auto bg-white shadow-lg rounded-2xl p-6 mt-6">
-      <h3 className="text-xl font-semibold text-center text-gray-800 mb-4">✨ Pessoas que estão tornando esse sonho possível ✨</h3>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 max-h-[300px] overflow-y-auto px-2">
-        {[
-          "Pedro Pereira Cecílio Ventura",
-          "Stephany Araujo da Silva Lobato",
-          "Nicolas Jesus Silveira",
-          "Thiago de Jesus Santana",
-          "Dielly Santana Farias",
-          "Nereide De Oliveira Costa",
-          "Pedro Henrique Stanzani De Freitas",
-          "Rodney Matheus Santos Fernandes",
-          "Sofia Maria Guimaraes Pavarina",
-          "Julia Diniz Do Nascimento Zanini",
-          "Maria Isabela Marques Pereira",
-          "Giovana Coelho Santana Leite",
-          "Florencio Vente Coha",
-          "Valentina Nieto Garcia",
-          "Leilany De Lima Alencar",
-          "Suzette Amoroso Ribeiro",
-          "Claudio Alves de Carvalho",
-          "Bruno Schmitt Locatelli",
-          "Railson da Silva Nunes",
-          "Adriana Rodrigues Julio",
-          "Flavio Augusto de Maia",
-          "Adriana Fonseca de Souza",
-          "Gustavo Colombi Ribolla",
-          "Mariana Abigair Souza Sabino",
-          "Luiz F A Steinberg",
-          "Guilherme Katayama Kaidei",
-          "Amanda Figueiredo Bittencourt",
-          "Henrique Fazzio Badin",
-          "Larissa Fernandes De Biagi"
-        ].map((nome, index) => (
-          <div
-            key={index}
-            className="bg-gray-100 rounded-xl px-4 py-2 text-sm text-gray-700 text-center shadow-sm hover:bg-yellow-100 transition duration-200"
-          >
-            {nome}
-          </div>
-        ))}
+        <PixCopyButton />
       </div>
-    </div>
 
-    <div align="center" className="mt-8 flex justify-center items-center gap-2 py-8">
-      <a href="https://www.instagram.com/_riquerocha_/" target="_blank">
-          <img
-          src="https://img.shields.io/static/v1?message=Instagram&logo=instagram&label=&color=E4405F&logoColor=white&labelColor=&style=for-the-badge"
-          height="35"
-          alt="instagram logo"
-          />
-      </a>
-      <a href="https://www.linkedin.com/in/henriquerochabomfim/" target="_blank">
-          <img
-          src="https://img.shields.io/static/v1?message=LinkedIn&logo=linkedin&label=&color=0077B5&logoColor=white&labelColor=&style=for-the-badge"
-          height="35"
-          alt="linkedin logo"
-          />
-      </a>
-    </div>
+      <CostTable />
+
+      <DonationsChart />
+
+      <h2 className="text-2xl font-bold mb-2 text-center py-2">Metas</h2>
+      <div className="flex justify-center items-center flex-col">
+        <p className="text-sm text-center text-gray-600 mb-2 py-3">
+          ☑ Meta até dia 15/04/2025: alcançar pelo menos 10% do valor arrecadado
+        </p>
+        <p className="text-sm text-center text-gray-600 mb-2 py-3">
+          ⌛Meta até dia 30/04/2025: alcançar pelo menos 20% do valor arrecadado
+        </p>
+      </div>
+
+      <div className="w-full max-w-4xl mx-auto bg-white shadow-lg rounded-2xl p-6 mt-6">
+        <h3 className="text-xl font-semibold text-center text-gray-800 mb-4">✨ Pessoas que estão tornando esse sonho possível ✨</h3>
+        <p className="text-lg text-center text-gray-700 mb-4">Total de doadores: {doadores.length}</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 max-h-[300px] overflow-y-auto px-2">
+          {doadores.map((nome, index) => (
+            <div
+              key={index}
+              className="bg-gray-100 rounded-xl px-4 py-2 text-sm text-gray-700 text-center shadow-sm hover:bg-yellow-100 transition duration-200"
+            >
+              {nome}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div align="center" className="mt-8 flex justify-center items-center gap-2 py-8">
+        <a href="https://www.instagram.com/_riquerocha_/" target="_blank">
+            <img
+            src="https://img.shields.io/static/v1?message=Instagram&logo=instagram&label=&color=E4405F&logoColor=white&labelColor=&style=for-the-badge"
+            height="35"
+            alt="instagram logo"
+            />
+        </a>
+        <a href="https://www.linkedin.com/in/henriquerochabomfim/" target="_blank">
+            <img
+            src="https://img.shields.io/static/v1?message=LinkedIn&logo=linkedin&label=&color=0077B5&logoColor=white&labelColor=&style=for-the-badge"
+            height="35"
+            alt="linkedin logo"
+            />
+        </a>
+      </div>
     </section>
   );
 }
