016bc4543198c2f691e96a5f418ad7794fa5218e
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2025-04-17 15:09:51 -0300

    Ocultando placar

--- henriquerochabomfim/src/app/components/gameSection.jsx
+++ henriquerochabomfim/src/app/components/gameSection.jsx
@@ -17,13 +17,13 @@ export default function GameSection() {
       <h2 className="text-xl font-bold">Criei um jogo de zumbis do zero para te entreter neste site, você pode jogá-lo abaixo:</h2>
       <h3 className="text-xl font mt-6">Obs: Só funciona em notebook e computador. Abaixe o volume do seu dispositivo caso fique muito alto</h3>
 
-      {topPlayer && (
+      {/* {topPlayer && (
         <div className="mt-4 bg-green-100 border border-green-400 text-green-700 px-4 py-2 rounded">
           🏆 <strong>Top 1:</strong> {topPlayer.name} — {topPlayer.score} pontos — {topPlayer.time_survived}s sobrevivido
         </div>
-      )}
+      )} */}
 
-      <SearchBar />
+      {/* <SearchBar /> */}
 
       <div className="flex justify-center mt-4">
         <iframe
